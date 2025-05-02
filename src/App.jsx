90dd021af1555adf6684c75ff1fc4ac8d1a21ea6
frenie8006 <seonitabatucan57@gmail.com>
2025-05-02 13:32:27 +0800

    Homepage part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Homepage from "./pages/Homepage";
+import Product from "./pages/Product";
+import Testimonial from "./pages/Testimonial";
+import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
   useEffect(function () {
-    fetch("https://dummyjson.com/products?limit=30&skip=0")
+    fetch("https://dummyjson.com/products?limit=8&skip=0")
       .then((res) => res.json())
-      .then((data) => console.log(data.products))
+      .then((data) => setItems(data.products))
       .catch((err) => console.log(err));
   }, []);
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Homepage />}></Route>
+        <Route index element={<Homepage items={items} />} />
+        <Route path="product" element={<Product />} />
+        <Route path="testimonial" element={<Testimonial />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
